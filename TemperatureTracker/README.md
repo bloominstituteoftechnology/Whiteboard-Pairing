9bdb5c891c06e7c6ae95e3fe4ef1c3fe24d5a813
Sean Chen <skypemaster007@gmail.com>
2018-05-11 10:05:00 -0700

    Add TemperatureTracker

---
+++ TemperatureTracker/README.md
@@ -0,0 +1,16 @@
+# Temperature Tracker
+Write a class TempTracker that tracks the max, min, mean, and mode of temperature values as they are inserted into the tracker. This class should have the following methods:
+
+ - insert() - records a new temperature.
+ - getMax() - returns the highest temperature we've seen so far.
+ - getMin() - returns the lowest temperature we've seen so far.
+ - getMean() - returns the mean of all temperatures we've seen so far.
+ - getMode() - returns a mode of all temperatures we've seen so far.
+
+Favor speeding up the get methods over the insert method. Aim to get each of the get methods down to constant runtime. 
+
+getMean() should return a float. The rest of the methods can return integers. Temperatures should be recorded in Fahrenheit, so we can assume a range of 0 to 140.
+
+If there is more than one mode, return any of the modes. 
+
+Analyze the time and space complexity of the methods in your solution.
---
+++ TemperatureTracker/model_solution.js
@@ -0,0 +1,81 @@
+class TempTracker {
+  constructor() {
+    // for mode
+    // populate occurrences array with 0s from 0 to 140
+    // 140 being the max temperature 
+    this.occurrences = Array(140).fill(0);
+    this.maxOccurrences = 0;
+    this.mode = null;
+
+    // for mean
+    this.numberOfReadings = 0;
+    this.totalSum = 0;
+    this.mean = null;
+
+    // for min and max
+    this.minTemp = null;
+    this.maxTemp = null;
+  }
+
+  insert(temp) {
+    // for mode
+    // increment the number of times this temp as appeared
+    this.occurrences[temp]++;
+    // update our maxOccurences and mode variables if necessary
+    if (this.occurrences[temp] > this.maxOccurrences) {
+      this.mode = temp;
+      this.maxOccurrences = this.occurrences[temp];
+    }
+
+    // for mean
+    this.numberOfReadings++;
+    this.totalSum += temp;
+    this.mean = this.totalSum / this.numberOfReadings;
+
+    // for min and max
+    if (this.maxTemp === null || temp > this.maxTemp) {
+      this.maxTemp = temp;
+    }
+    if (this.minTemp === null || temp < this.minTemp) {
+        this.minTemp = temp;
+    }
+  }
+
+  getMax() {
+    return this.maxTemp;
+  }
+
+  getMin() {
+    return this.minTemp;
+  }
+
+  getMean() {
+    return this.mean;
+  }
+
+  getMode() {
+    return this.mode;
+  }
+}
+
+/* Some console.log tests */
+const tracker = new TempTracker();
+tracker.insert(32);
+
+console.log("mean: ", tracker.getMean());   // should print 32
+console.log("min: ", tracker.getMin());     // should print 32
+console.log("max: ", tracker.getMax());     // should print 32
+console.log("mode: ", tracker.getMode());   // should print 32
+
+tracker.insert(135);
+
+console.log("mean: ", tracker.getMean());   // should print 83.5
+console.log("min: ", tracker.getMin());     // should print 32
+console.log("max: ", tracker.getMax());     // should print 135
+
+tracker.insert(135);
+
+console.log("mean: ", tracker.getMean());   // should print 100.66666666666667
+console.log("min: ", tracker.getMin());     // should print 32
+console.log("max: ", tracker.getMax());     // should print 135
+console.log("mode: ", tracker.getMode());   // should print 135
