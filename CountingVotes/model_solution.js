1d080a9e0bb4e03b577a3a09ebe52605f414665d
AJ Genung <DragonRydrz@me.com>
2018-09-16 16:15:00 -0400

    updated countingVotes optimal solution

--- CountingVotes/model_solution.js
+++ CountingVotes/model_solution.js
@@ -1,48 +1,45 @@
 /*
-  O(2n) time complexity and O(n) space complexity
+  O(n) time complexity and O(n) space complexity
 */
 
-function countVotes(votes) {
-  // use a hash to store names and their number of votes
-  const hash = {};
+function countVotes(arr) {
+  const counts = {};
+  let maxVotes = 0;
+  let winner = '';
+  // Iterate over entire array
+  arr.forEach(vote => {
 
-  // insert by name and number of occurrences into our map
-  votes.forEach(vote => {
-    // if name doesn't exist in hash, initialize it
-    if (!hash[vote]) {
-      hash[vote] = 1;
-    }
+    // If name doesnt exist in counts yet, add it with a value of 0
+    if (!counts[vote]) counts[vote] = 0;
 
-    // if it does exist, increment the count
-    hash[vote]++;
-  });
+    // Increment the count of the name
+    counts[vote]++;
 
-  let winner;
-  let winnerCount = 0;
+    // Check to see if current name has more votes then the current max
+    if (counts[vote] > maxVotes) {
 
-  // loop through all our hash entries
-  Object.entries(hash).forEach(([name, count]) => {
-    // if we've found a higher number of votes
-    // update `winner` to be that person
-    // don't forget to update `winnerCount`
-    if (count > winnerCount) {
-      winnerCount = count;
-      winner = name;
-    }
-    // if there's a tie, pick the person whose
-    // names shows up later in the alphabet
-    else if (count === winnerCount) {
-      winner = name > winner ? name : winner;
+      // Sets the max votes to the current names votes if larger
+      maxVotes = counts[vote];
+
+      // Since this name has more votes, its currently the winner
+      winner = vote;
+
+      // Checks to see if current name is tied to the winner
+    } else if (counts[vote] === maxVotes) {
+
+      // if tied, sets the winner to the name that is greater (last alphabetically)
+      if (vote > winner) winner = vote;
     }
   });
-
   return winner;
 }
 
-console.log(countVotes(
-  ['veronica', 'mary', 'alex', 'james', 'mary', 'michael', 'alex', 'michael']
-));  // should print 'michael'
+console.log(
+  countVotes(
+    ['veronica', 'mary', 'alex', 'james', 'mary', 'michael', 'alex', 'michael',]
+  )); // should print 'michael'
 
-console.log(countVotes(
-  ["john", "johnny", "jackie", "johnny", "john", "jackie", "jamie", "jamie", "john","johnny", "jamie", "johnny", "john"]
-));   // should print 'john'
+console.log(
+  countVotes(
+    ['john', 'johnny', 'jackie', 'johnny', 'john', 'jackie', 'jamie', 'jamie', 'john', 'johnny', 'jamie', 'johnny', 'john',]
+  )); // should print 'john'
