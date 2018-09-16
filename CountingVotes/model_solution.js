2f36ff64c974d8e2bee4a89235d471a073ec9e49
AJ Genung <DragonRydrz@me.com>
2018-09-16 16:17:31 -0400

    2nd example should return `johnny`

--- CountingVotes/model_solution.js
+++ CountingVotes/model_solution.js
@@ -8,7 +8,6 @@ function countVotes(arr) {
   let winner = '';
   // Iterate over entire array
   arr.forEach(vote => {
-
     // If name doesnt exist in counts yet, add it with a value of 0
     if (!counts[vote]) counts[vote] = 0;
 
@@ -17,7 +16,6 @@ function countVotes(arr) {
 
     // Check to see if current name has more votes then the current max
     if (counts[vote] > maxVotes) {
-
       // Sets the max votes to the current names votes if larger
       maxVotes = counts[vote];
 
@@ -26,7 +24,6 @@ function countVotes(arr) {
 
       // Checks to see if current name is tied to the winner
     } else if (counts[vote] === maxVotes) {
-
       // if tied, sets the winner to the name that is greater (last alphabetically)
       if (vote > winner) winner = vote;
     }
@@ -35,11 +32,32 @@ function countVotes(arr) {
 }
 
 console.log(
-  countVotes(
-    ['veronica', 'mary', 'alex', 'james', 'mary', 'michael', 'alex', 'michael',]
-  )); // should print 'michael'
+  countVotes([
+    'veronica',
+    'mary',
+    'alex',
+    'james',
+    'mary',
+    'michael',
+    'alex',
+    'michael',
+  ])
+); // should print 'michael'
 
 console.log(
-  countVotes(
-    ['john', 'johnny', 'jackie', 'johnny', 'john', 'jackie', 'jamie', 'jamie', 'john', 'johnny', 'jamie', 'johnny', 'john',]
-  )); // should print 'john'
+  countVotes([
+    'john',
+    'johnny',
+    'jackie',
+    'johnny',
+    'john',
+    'jackie',
+    'jamie',
+    'jamie',
+    'john',
+    'johnny',
+    'jamie',
+    'johnny',
+    'john',
+  ])
+); // should print 'johnny'
