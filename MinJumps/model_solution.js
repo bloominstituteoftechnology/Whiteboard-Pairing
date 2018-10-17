631c09fecae1e53d056a65dbf8d8b26afa6e988d
Sean Chen <skypemaster007@gmail.com>
2018-10-17 11:57:10 -0700

    Add more console.log tests

--- MinJumps/model_solution.js
+++ MinJumps/model_solution.js
@@ -2,28 +2,28 @@
   Naive recursive solution that recurses along
   each possible jump and branches out from there
 */
-function minJumps(arr, start=0, end=arr.length-1) {
-  // base case: when start and end are at the same spot
-  if (start === end) {
-    return 0;
-  }
-  // when nothing is reachable, return infinity
-  if (arr[start] === 0) {
-    return Infinity;
-  }
-  // traverse through all the spots reachable by
-  // arr[start], recursively getting the min number
-  // of jumps needed to reach arr[end]
-  let min = Infinity;
-  for (let i = start + 1; i <= end && i <= start + arr[start]; i++) {
-    const jumps = minJumps(arr, start, end);
-    if (jumps !== Infinity && jumps + 1 < min) {
-      min = jumps + 1;
-    }
-  }
+// function minJumps(arr, start=0, end=arr.length-1) {
+//   // base case: when start and end are at the same spot
+//   if (start === end) {
+//     return 0;
+//   }
+//   // when nothing is reachable, return infinity
+//   if (arr[start] === 0) {
+//     return Infinity;
+//   }
+//   // traverse through all the spots reachable by
+//   // arr[start], recursively getting the min number
+//   // of jumps needed to reach arr[end]
+//   let min = Infinity;
+//   for (let i = start + 1; i <= end && i <= start + arr[start]; i++) {
+//     const jumps = minJumps(arr, start, end);
+//     if (jumps !== Infinity && jumps + 1 < min) {
+//       min = jumps + 1;
+//     }
+//   }
 
-  return min;
-}
+//   return min;
+// }
 
 /*
   Solution that utilizes dynamic programming in order to
@@ -57,3 +57,4 @@ function minJumps(arr, n=arr.length) {
 console.log(minJumps([1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9]));  // should print 3
 console.log(minJumps([1, 3, 6, 1, 0, 9]));  // should print 3
 console.log(minJumps([2, 0, 0, 5, 8, 1, 7, 4, 9, 12, 1]));  // should print Infinity
+console.log(minJumps([1, 3, 6, 3, 2, 3, 6, 8, 9, 5]));  // should print 4
