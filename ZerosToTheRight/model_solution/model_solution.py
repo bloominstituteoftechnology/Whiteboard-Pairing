165c4551d3832e11b7920a1bf6d23afe8d4211ab
Sean Chen <skypemaster007@gmail.com>
2019-05-28 13:55:31 -0700

    Get working python implementation of ZerosToTheRight problem

--- ZerosToTheRight/model_solution/model_solution.py
+++ ZerosToTheRight/model_solution/model_solution.py
@@ -2,27 +2,23 @@ def zeros_to_the_right(arr):
     left = 0
     right = len(arr) - 1
     n_zeros = 0
-    did_swap = False
 
-    while left < right:
+    while left <= right:
         if arr[left] == 0 and arr[right] != 0:
             arr[left], arr[right] = arr[right], arr[left]
             left += 1
             right -= 1
-            did_swap = True
+            n_zeros += 1
         else:
             if arr[left] != 0:
                 left += 1
             if arr[right] == 0:
+                n_zeros += 1
                 right -= 1
     print(arr)
+    return len(arr) - n_zeros
 
-    if did_swap:
-        return len(arr) - right
-    else:
-        return len(arr) - left
-
-print("Number of non-zero integers: ", zeros_to_the_right([0, 3, 1, 0, -2]))  
+print("Number of non-zero integers: ", zeros_to_the_right([0, 3, 1, 0, -2])) 
 # should print:
 # [-2, 3, 1, 0, 0]
 # Number of non-zero integers: 3
@@ -41,3 +37,8 @@ print("Number of non-zero integers: ", zeros_to_the_right([0, 0, 0, 0, 0]))
 # should print:
 # [0, 0, 0, 0, 0]
 # Number of non-zero integers: 0
+
+print("Number of non-zero integers: ", zeros_to_the_right([0, 0, 0, 0, 3, 2, 1]))
+# should print:
+# [1, 2, 3, 0, 0, 0, 0]
+# Number of non-zero integers: 3
