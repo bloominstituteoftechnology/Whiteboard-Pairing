c3ecbad2d27d1d92a4cc5bd25a6f3ad188d6a27f
Mark Oliver <mark.c.oliver@gmail.com>
2019-06-05 01:27:31 -0400

    model solution that works

--- MatrixSpiralCopy/sri_test.py
+++ MatrixSpiralCopy/sri_test.py
@@ -2,6 +2,7 @@ import unittest
 import random
 from spiralCopy import spiralCopy
 from model_solution.model_solution import spiralCopy as model_spiralCopy
+from test_model import matrix_spiral_copy
 import logging
 import sys
 
@@ -21,8 +22,8 @@ class IterativeSortingTest(unittest.TestCase):
         self.assertEqual(spiralCopy(arr1),
                          [1, 2, 3, 4, 5, 10, 15, 20, 19, 18,
                           17, 16, 11, 6, 7, 8, 9, 14, 13, 12])
-        log.info('test model spiralCopy')
-        self.assertEqual(model_spiralCopy(arr1),
+        log.info('test test_model spiralCopy')
+        self.assertEqual(matrix_spiral_copy(arr1),
                          [1, 2, 3, 4, 5, 10, 15, 20, 19, 18,
                           17, 16, 11, 6, 7, 8, 9, 14, 13, 12])                          
 
@@ -43,8 +44,8 @@ class IterativeSortingTest(unittest.TestCase):
         random.sample(range(200), 50),
         random.sample(range(200), 50),
         random.sample(range(200), 50)]                                     
-        res2 = model_spiralCopy(arr2) 
-        log.info(f'{15 * 50}: my {len(spiralCopy(arr2))} model  {len(model_spiralCopy(arr2))}')
+        res2 = matrix_spiral_copy(arr2) 
+        log.info(f'{15 * 50}: my {len(spiralCopy(arr2))} model  {len(matrix_spiral_copy(arr2))}')
         self.assertEqual(spiralCopy(arr2),res2)
 
 if __name__ == '__main__':
