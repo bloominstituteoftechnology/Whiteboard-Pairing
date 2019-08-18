2bfcc37a5a6f7a6aeee24f8c39312d1eb33820e6
benjchand <benchand@gmail.com>
2019-08-18 15:09:43 -0400

    Done with Pseudocode for BalancedBracket.

---
+++ WhiteboardPairing/BalancedBrackets/BalancedBracketsPractice.py
@@ -0,0 +1,7 @@
+# Start a formula that takes in the string of variables.
+# Iterate over the characters in the string.
+# Keep track of each of the types of characters, + for open, - for close.
+# Check the values for each of the characters to see if they are 0 or not.
+# If all are zero, return true.
+# If not zero, return false.
+# return character_counter + character_counter + character_counter == 0
