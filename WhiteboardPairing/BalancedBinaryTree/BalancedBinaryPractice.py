2c68c0603efbe4f0a77ffe92e394c585bf11d034
benjchand <benchand@gmail.com>
2019-08-17 23:17:45 -0400

    Code works well, rewrote it to practice.

--- WhiteboardPairing/BalancedBinaryTree/BalancedBinaryPractice.py
+++ WhiteboardPairing/BalancedBinaryTree/BalancedBinaryPractice.py
@@ -1,19 +1,44 @@
+# Make sure that an empty tree return True
+
 
 def checkBalance(rootnode):
     if rootnode == None:
         return True
+# Find the depth of the minimum tree.
 
     def minDepth(node):
         if node == None:
             return 0
+    # RECURSION
         return 1 + min(minDepth(node.left), minDepth(node.right))
+# Find the depth of the maximum tree.
 
     def maxDepth(node):
         if node == None:
             return 0
+    # RECURSION
         return 1 + max(maxDepth(node.left), maxDepth(node.right))
 
+# If the maxdepth - mindepth == 0 , return true, else return false.
     return maxDepth(rootnode) - minDepth(rootnode) == 0
+    # Really can return the statement, since that will be true or false.
+
+
+# def checkBalance(rootnode):
+#     if rootnode == None:
+#         return True
+
+#     def minDepth(node):
+#         if node == None:
+#             return 0
+#         return 1 + min(minDepth(node.left), minDepth(node.right))
+
+#     def maxDepth(node):
+#         if node == None:
+#             return 0
+#         return 1 + max(maxDepth(node.left), maxDepth(node.right))
+
+#     return maxDepth(rootnode) - minDepth(rootnode) == 0
 
 
 class BinaryTreeNode:
