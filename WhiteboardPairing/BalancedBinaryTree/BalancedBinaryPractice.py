61e16fa633dcba8e34042a647215eb14961bcc9a
benjchand <benchand@gmail.com>
2019-08-17 23:11:36 -0400

    Code works well.

--- WhiteboardPairing/BalancedBinaryTree/BalancedBinaryPractice.py
+++ WhiteboardPairing/BalancedBinaryTree/BalancedBinaryPractice.py
@@ -4,13 +4,38 @@ def checkBalance(rootnode):
         return True
 
     def minDepth(node):
-        if node == 0:
+        if node == None:
             return 0
         return 1 + min(minDepth(node.left), minDepth(node.right))
 
     def maxDepth(node):
-        if node == 0:
+        if node == None:
             return 0
         return 1 + max(maxDepth(node.left), maxDepth(node.right))
 
     return maxDepth(rootnode) - minDepth(rootnode) == 0
+
+
+class BinaryTreeNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insertLeft(self, value):
+        self.left = BinaryTreeNode(value)
+        return self.left
+
+    def insertRight(self, value):
+        self.right = BinaryTreeNode(value)
+        return self.right
+
+
+root = BinaryTreeNode(5)
+print(checkBalance(root))   # should print True
+
+root.insertLeft(10)
+print(checkBalance(root))   # should print False
+
+root.insertRight(11)
+print(checkBalance(root))   # should print True
