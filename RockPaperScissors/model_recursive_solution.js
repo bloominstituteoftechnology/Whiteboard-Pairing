16ed5400589854fc3230cc28430734f7407daa26
samsisle <samko9522@gmail.com>
2019-12-05 21:36:28 -0800

    Update model_solution to model_recursive_solution

--- RockPaperScissors/model_recursive_solution.js
+++ RockPaperScissors/model_recursive_solution.js
@@ -2,19 +2,19 @@ function rockPaperScissors(n) {
   // outcomes array is the outer array that will
   // hold all of the smaller arrays
   const outcomes = [];
-  const plays = ['rock', 'paper', 'scissors'];
+  const plays = ["rock", "paper", "scissors"];
   // inner recursive helper function
-  function findOutcome (roundsLeft, result) {
+  function findOutcome(playersLeft, result) {
     // base case
-    if (roundsLeft === 0) {
+    if (playersLeft === 0) {
       outcomes.push(result);
       return;
     }
     // move towards the base case by adding the next play
     // and recursively calling this function by decrementing
-    // the `roundsLeft` argument
-    plays.forEach((play) => {
-      findOutcome(roundsLeft - 1, result.concat(play));
+    // the `playersLeft` argument
+    plays.forEach(play => {
+      findOutcome(playersLeft - 1, result.concat(play));
     });
   }
   // don't forget to make the initial call
@@ -24,13 +24,13 @@ function rockPaperScissors(n) {
 
 /* Some console.log tests */
 const rv1 = rockPaperScissors(2);
-console.log(rv1);   // should print [[rock, rock], [rock, paper], [rock, scissors], [paper, rock], [paper, paper], [paper, scissors], [scissors, rock], [scissors, paper], [scissors, scissors]]
+console.log(rv1); // should print [[rock, rock], [rock, paper], [rock, scissors], [paper, rock], [paper, paper], [paper, scissors], [scissors, rock], [scissors, paper], [scissors, scissors]]
 
 const rv2 = rockPaperScissors(3);
-console.log(rv2.length);    // should print 27
+console.log(rv2.length); // should print 27
 
 const rv3 = rockPaperScissors(4);
-console.log(rv3.length);    // should print 81
+console.log(rv3.length); // should print 81
 
 const rv4 = rockPaperScissors(5);
-console.log(rv4.length);    // should print 243
+console.log(rv4.length); // should print 243
