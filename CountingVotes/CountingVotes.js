32a9df98edb664fdd33cfddb7ef901b9ad4b83d5
jawad727 <jawaddhussein@gmail.com>
2020-01-29 18:56:41 -0800

    implemented a singly linked list with all of its methods

--- CountingVotes/CountingVotes.js
+++ CountingVotes/CountingVotes.js
@@ -6,14 +6,18 @@ function countVotes(arr) {
     winner = ""
     record = 0
     for (i=0; i<arr.length; i++) {
+        
       if (!names[arr[i]]) {
         names[arr[i]] = 0
       }
       names[arr[i]] += 1
           if (names[arr[i]] > record) {
+
         record = names[arr[i]];
         winner = arr[i];
+        
       } else if (names[arr[i]] === record) {
+
         if (arr[i] > winner) winner = arr[i];
       }
     }
